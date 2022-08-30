07bddaa477f19a78f3ee360527447d5072d0707c
ShivaniYS <87171146+ShivaniYS@users.noreply.github.com>
2022-08-30 14:25:46 +0530

    Update missing_number.cpp

--- missing_number.cpp
+++ missing_number.cpp
@@ -18,3 +18,54 @@ int main(){
     cout<<missingNumber;
     return 0;
 }
+
+
+
+USING TEMPLATE
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& arr, int N) {
+        // Your code goes here
+        int total = 0;
+        int d = 0;
+        total = (N) * (N+1)/2;
+        int newSum = 0;
+    
+    for(int i=0;i<N-1;i++)
+       newSum = newSum + arr[i];
+       d = total - newSum;
+    return d;
+    }
+     
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
